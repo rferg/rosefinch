f3ebc5a45ca5d1fa5314d32144b172036faeedd9
rferg <rmferguson77@gmail.com>
2020-08-09 11:31:40 -0400

    chord fit fitness function

---
+++ src/genetic-algorithm/fitness/chord-fit-factory.ts
@@ -0,0 +1,45 @@
+import { Pitch } from '../../common/pitch'
+import { Population } from '../population'
+import { GeneUtil } from '../../common/gene-util'
+import { Uint8 } from '../../common/uint8'
+
+export function chordFitFactory({
+    chords = {}
+}: {
+    chords: { [geneIndex: number]: Pitch[] }
+}): (population: Population) => Int8Array {
+    return (population) => {
+        const results = new Int8Array(population.size)
+        for (let genomeIndex = 0; genomeIndex < population.size; genomeIndex++) {
+            const genome = population.get(genomeIndex)
+            let currentChord: Pitch[] = []
+            let totalNotes = 0
+            let totalFittingNotes = 0
+            let lastNote: Pitch | undefined
+            for (let geneIndex = 0; geneIndex < genome.length; geneIndex++) {
+                let pitch: Pitch | undefined = GeneUtil.getPitch(genome[geneIndex] as Uint8)
+                currentChord = chords[geneIndex] || currentChord
+                if (pitch === Pitch.Hold) {
+                    pitch = lastNote
+                } else {
+                    lastNote = pitch
+                }
+                totalFittingNotes += (pitchIsInChord(pitch, currentChord) ? 1 : 0)
+                totalNotes += (pitch !== Pitch.Rest ? 1 : 0)
+            }
+
+            results[genomeIndex] = calculateFitness(totalNotes, totalFittingNotes)
+        }
+        return results
+    }
+}
+
+function pitchIsInChord(pitch: Pitch | undefined, chord: Pitch[]): boolean {
+    if (!pitch) { return false }
+    return (chord || []).indexOf(pitch) !== -1
+}
+
+function calculateFitness(totalNotes: number, totalFittingNotes: number): number {
+    if (!totalNotes) { return 0 }
+    return Math.round((totalFittingNotes / totalNotes) * 100)
+}
---
+++ test/genetic-algorithm/fitness/chord-fit.spec.ts
@@ -0,0 +1,202 @@
+import { Pitch } from '../../../src/common/pitch'
+import { chordFitFactory } from '../../../src/genetic-algorithm/fitness/chord-fit-factory'
+import { Population } from '../../../src/genetic-algorithm/population'
+
+describe('chordFit', () => {
+    it('should return array of same size as population', () => {
+        const population = new Population({
+            size: 5,
+            genomeSize: 4,
+            geneFactory: () => Pitch.C
+        })
+        const chords = {}
+
+        const fitnesses = chordFitFactory({ chords })(population)
+
+        expect(fitnesses.length).toEqual(population.size)
+    })
+
+    it('should return 0 if all rests', () => {
+        const population = new Population({
+            size: 3,
+            genomeSize: 4,
+            geneFactory: () => Pitch.Rest
+        })
+        const chords = { 0: [ Pitch.C, Pitch.B ] }
+
+        const fitnesses = chordFitFactory({ chords })(population)
+
+        for (const fitness of fitnesses) {
+            expect(fitness).toBe(0)
+        }
+    })
+
+    it('should return 0 if no chords', () => {
+        const population = new Population({
+            size: 3,
+            genomeSize: 4,
+            geneFactory: () => Pitch.C
+        })
+        const chords = {}
+
+        const fitnesses = chordFitFactory({ chords })(population)
+
+        for (const fitness of fitnesses) {
+            expect(fitness).toBe(0)
+        }
+    })
+
+    it('should return 100 if all pitches in chords', () => {
+        const population = new Population({
+            size: 3,
+            genomeSize: 4,
+            geneFactory: () => Pitch.C
+        })
+        const chords = { 0: [ Pitch.C ] }
+
+        const fitnesses = chordFitFactory({ chords })(population)
+
+        for (const fitness of fitnesses) {
+            expect(fitness).toBe(100)
+        }
+    })
+
+    it('should count Pitch.Holds as last non-Hold pitch', () => {
+        const population = new Population({
+            size: 1,
+            genomeSize: 4,
+            geneFactory: (i: number) => i ? Pitch.Hold : Pitch.C
+        })
+        const chords = { 0: [ Pitch.C ] }
+
+        const fitness = chordFitFactory({ chords })(population)[0]
+
+        expect(fitness).toBe(100)
+    })
+
+    it('should count initial Pitch.Hold as undefined note', () => {
+        const population = new Population({
+            size: 1,
+            genomeSize: 4,
+            geneFactory: (i: number) => i ? Pitch.C : Pitch.Hold
+        })
+        const chords = { 0: [ Pitch.C ] }
+
+        const fitness = chordFitFactory({ chords })(population)[0]
+
+        const expectedFitness = Math.round(((population.genomeSize - 1) / population.genomeSize) * 100)
+        expect(fitness).toBe(expectedFitness)
+    })
+
+    it('should not count Pitch.Rests towards note total', () => {
+        const population = new Population({
+            size: 1,
+            genomeSize: 4,
+            geneFactory: (i: number) => i ? Pitch.C : Pitch.Rest
+        })
+        const chords = { 0: [ Pitch.C ] }
+
+        const fitness = chordFitFactory({ chords })(population)[0]
+
+        expect(fitness).toBe(100)
+    })
+
+    it('should count Pitch.Holds across chord boundaries correctly', () => {
+        const population = new Population({
+            size: 1,
+            genomeSize: 4,
+            geneFactory: (i: number) => i ? Pitch.Hold : Pitch.C
+        })
+        const chords = { 0: [ Pitch.C ], 2: [ Pitch.B ] }
+
+        const fitness = chordFitFactory({ chords })(population)[0]
+
+        expect(fitness).toBe(50)
+    })
+
+    const cases: { chords: { [i: number]: Pitch[] }, pitches: Pitch[], expectedFitness: number }[] = [
+        {
+            chords: {
+                0: [ Pitch.C, Pitch.E, Pitch.F ],
+                2: [ Pitch.B, Pitch.F, Pitch.G ]
+            },
+            pitches: [
+                Pitch.Bb,
+                Pitch.E,
+                Pitch.F,
+                Pitch.Hold
+            ],
+            expectedFitness: 75
+        },
+        {
+            chords: {
+                0: [ Pitch.C, Pitch.E, Pitch.F ],
+                2: [ Pitch.B, Pitch.F, Pitch.G ]
+            },
+            pitches: [
+                Pitch.E,
+                Pitch.E,
+                Pitch.Hold,
+                Pitch.Hold
+            ],
+            expectedFitness: 50
+        },
+        {
+            chords: {
+                0: [ Pitch.C, Pitch.E, Pitch.F ],
+                2: [ Pitch.B, Pitch.F, Pitch.G ]
+            },
+            pitches: [
+                Pitch.Rest,
+                Pitch.E,
+                Pitch.Rest,
+                Pitch.Hold
+            ],
+            expectedFitness: 100
+        },
+        {
+            chords: {
+                0: [ Pitch.C, Pitch.E, Pitch.F ],
+                2: [ Pitch.B, Pitch.F, Pitch.G ]
+            },
+            pitches: [
+                Pitch.Bb,
+                Pitch.E,
+                Pitch.Rest,
+                Pitch.Hold
+            ],
+            expectedFitness: 50
+        },
+        {
+            chords: {
+                0: [ Pitch.C, Pitch.E, Pitch.F ],
+                2: [ Pitch.B, Pitch.F, Pitch.G ]
+            },
+            pitches: [
+                Pitch.Bb,
+                Pitch.C,
+                Pitch.C,
+                Pitch.Rest
+            ],
+            expectedFitness: 33
+        }
+    ]
+
+    const chordsToString = (chords: { [i: number]: Pitch[] }) => {
+        return Object.keys(chords).map(index => `${index}: ${chords[Number.parseInt(index)]}`).join(' | ')
+    }
+
+    cases.forEach(({ chords, pitches, expectedFitness }) => {
+        it(`should return ${expectedFitness} with chords: ${chordsToString(chords)} for genome: ${pitches}`, () => {
+            const population = new Population({
+                size: 1,
+                genomeSize: pitches.length,
+                geneFactory: (i: number) => pitches[i]
+            })
+
+            const fitness = chordFitFactory({ chords })(population)[0]
+
+            expect(fitness).toBe(expectedFitness)
+        })
+    })
+})
