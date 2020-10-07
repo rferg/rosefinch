6abafbfb77c7fb974a595649d5fdf3937bdcf5df
rferg <rmferguson77@gmail.com>
2020-10-07 17:42:13 -0400

    update tests for RepresentativesElement to account for playback and playback options

--- test/ui/representatives/representatives.element.spec.ts
+++ test/ui/representatives/representatives.element.spec.ts
@@ -2,7 +2,7 @@ import { elementUpdated, fixture } from '@open-wc/testing-helpers'
 import { html, property } from 'lit-element'
 import { SerializedGeneticAlgorithmOptions } from '../../../src/genetic-algorithm'
 import { RepresentativeGenesService } from '../../../src/services/pipeline'
-import { PlaybackService } from '../../../src/services/playback'
+import { Instrument, PlaybackOptions, PlaybackService } from '../../../src/services/playback'
 import { RepresentativeGenesState, StateMediatorService, StateSubscription, StateTopic, UpdateStateEvent } from '../../../src/services/state'
 import {
     GeneticAlgorithmOptionsRepository,
@@ -14,6 +14,7 @@ import { BaseElement } from '../../../src/ui/core/base-element'
 import { Router } from '../../../src/ui/core/router'
 import { cancelEventType } from '../../../src/ui/options/cancel-event-type'
 import { FormSubmitEvent } from '../../../src/ui/options/form-submit-event'
+import { PopupContent } from '../../../src/ui/representatives/popup-content'
 import { RepresentativeElement } from '../../../src/ui/representatives/representative.element'
 import { RepresentativesElement } from '../../../src/ui/representatives/representatives.element'
 import { ContainerElementStub } from '../../helpers/container-element-stub'
@@ -35,6 +36,9 @@ class EditRepresentativeElementStub extends BaseElement {
 
     @property()
     rating?: number
+
+    @property({ reflect: true, type: Boolean })
+    playing = false
 }
 
 class RepresentativeElementStub extends BaseElement {
@@ -75,6 +79,8 @@ describe('RepresentativesElement', () => {
         'EventTarget',
         [ 'dispatchEvent' ])
     const playbackSpy = jasmine.createSpyObj<PlaybackService>('PlaybackService', [ 'setupSequence' ])
+    let playSpy: jasmine.Spy
+    let pauseSpy: jasmine.Spy
 
     let el: RepresentativeElement
 
@@ -103,6 +109,13 @@ describe('RepresentativesElement', () => {
         optionsRepoSpy.get.calls.reset()
         summaryRepoSpy.get.calls.reset()
         eventSpy.dispatchEvent.calls.reset()
+        playbackSpy.setupSequence.calls.reset()
+        playSpy = jasmine.createSpy('PlaySpy')
+        pauseSpy = jasmine.createSpy('PauseSpy')
+        playbackSpy.setupSequence.and.returnValue(Promise.resolve({
+            play: playSpy,
+            pause: pauseSpy
+        }))
 
         el = await fixture(html`<rf-representatives-test></rf-representatives-test>`)
     })
@@ -187,7 +200,8 @@ describe('RepresentativesElement', () => {
                     await elementUpdated(el)
 
                     expect((el.shadowRoot
-                        ?.querySelector('rf-representatives-header') as RepresentativesHeaderElementStub).generation)
+                        ?.querySelector(RepresentativesHeaderElementStub.is) as RepresentativesHeaderElementStub)
+                        .generation)
                         .toEqual(expectedGeneration)
                 })
 
@@ -283,7 +297,8 @@ describe('RepresentativesElement', () => {
         }
 
         beforeEach(() => {
-            const head = el.shadowRoot?.querySelector('rf-representatives-header') as RepresentativesHeaderElementStub
+            const head = el.shadowRoot
+                ?.querySelector(RepresentativesHeaderElementStub.is) as RepresentativesHeaderElementStub
             if (!head) { throw new Error('missing rf-representatives-header') }
             headerEl = head
         })
@@ -303,7 +318,7 @@ describe('RepresentativesElement', () => {
 
             const popupEl = getPopup()
             expect(popupEl.show).toBeTrue()
-            expect(popupEl.querySelector('rf-playback-options')).toBeDefined()
+            expect(popupEl.querySelector(PlaybackOptionsElementStub.is)).toBeDefined()
         })
 
         it('should display run confirm form if emit event with "run"', async () => {
@@ -405,7 +420,7 @@ describe('RepresentativesElement', () => {
             editRepEl.dispatchEvent(new CustomEvent<number>('rating-change', { detail: expectedRatings[0] }))
             await elementUpdated(el)
             const headerEl = el.shadowRoot
-                ?.querySelector('rf-representatives-header') as RepresentativesHeaderElementStub
+                ?.querySelector(RepresentativesHeaderElementStub.is) as RepresentativesHeaderElementStub
             if (!headerEl) { throw new Error('missing rf-representatives-header') }
             headerEl.dispatchEvent(new CustomEvent<string>('show-popup', { detail: 'run' }))
             await elementUpdated(el)
@@ -421,6 +436,147 @@ describe('RepresentativesElement', () => {
                 { numberOfGenerations, geneticAlgorithmId, userRepresentativeRatings: expectedRatings }
             ))
             expect(routerSpy.navigate).toHaveBeenCalledWith('/run')
+        })
+    })
+
+    describe('on play event from EditRepresentativeElement', () => {
+
+        beforeEach(async () => {
+            initiateWithGenomes([ [ 1 ] ], 'a')
+            await elementUpdated(el)
+        })
+
+        it('should setup sequence, get controls and play, if first call', async () => {
+            const editRepEl = el.shadowRoot
+                ?.querySelector('rf-edit-representative') as EditRepresentativeElementStub
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+            await elementUpdated(el)
+
+            expect(playbackSpy.setupSequence).toHaveBeenCalled()
+            expect(playSpy).toHaveBeenCalled()
+        })
+
+        it('should only setup sequence once if called twice with same genome', async () => {
+            const editRepEl = el.shadowRoot
+                ?.querySelector('rf-edit-representative') as EditRepresentativeElementStub
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+            await elementUpdated(el)
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+
+            expect(playbackSpy.setupSequence).toHaveBeenCalledTimes(1)
+            expect(playSpy).toHaveBeenCalledTimes(2)
+        })
+
+        it('should pass a note change callback that sets EditRepresentativeElement.playing as !isDone', async () => {
+            const editRepEl = el.shadowRoot
+                ?.querySelector('rf-edit-representative') as EditRepresentativeElementStub
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+            await elementUpdated(el)
+
+            const callback = playbackSpy.setupSequence.calls.mostRecent().args[0].callbacks?.onNoteChange
+            if (!callback) { throw new Error('on note change callback missing') }
+
+            callback('', 0, false)
+            await elementUpdated(el)
+            expect(editRepEl.playing).toBeTrue()
+
+            callback('', 0, true)
+            await elementUpdated(el)
+            expect(editRepEl.playing).toBeFalse()
+        })
+    })
+
+    describe('on pause event', () => {
+        let editRepEl: EditRepresentativeElementStub
+
+        beforeEach(async () => {
+            initiateWithGenomes([ [ 1 ] ], 'a')
+            await elementUpdated(el)
+            editRepEl = el.shadowRoot
+                ?.querySelector('rf-edit-representative') as EditRepresentativeElementStub
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+            await elementUpdated(el)
+        })
+
+        it('should call pause on playback controls', async () => {
+            editRepEl.dispatchEvent(new CustomEvent('pause'))
+            await elementUpdated(el)
+
+            expect(pauseSpy).toHaveBeenCalled()
+        })
+
+        it('should update EditRepresentativeElement playing property to false', async () => {
+            // Ensure playing has been set to true.
+            const callback = playbackSpy.setupSequence.calls.mostRecent().args[0].callbacks?.onNoteChange
+            if (!callback) { throw new Error('on note change callback missing') }
+            callback('', 0, false)
+
+            editRepEl.dispatchEvent(new CustomEvent('pause'))
+            await elementUpdated(el)
+
+            expect(editRepEl.playing).toBeFalse()
+        })
+    })
+
+    describe('on playback options change event', () => {
+        let playbackOptionsEl: PlaybackOptionsElementStub
+
+        beforeEach(async () => {
+            initiateWithGenomes([ [ 1 ] ], 'a')
+            await elementUpdated(el)
+            const headerEl = el.shadowRoot
+                ?.querySelector(RepresentativesHeaderElementStub.is) as RepresentativesHeaderElementStub
+            headerEl.dispatchEvent(new CustomEvent<PopupContent>('show-popup', { detail: 'playback' }))
+            await elementUpdated(el)
+            playbackOptionsEl = el.shadowRoot
+                ?.querySelector(PlaybackOptionsElementStub.is) as PlaybackOptionsElementStub
+        })
+
+        it('should update playback options', async () => {
+            const newOptions: PlaybackOptions = {
+                bpm: 1,
+                instrument: Instrument.Synth,
+                loop: true
+            }
+            playbackOptionsEl.dispatchEvent(new FormSubmitEvent<PlaybackOptions>({ value: newOptions }))
+            await elementUpdated(el)
+
+            // Check that options have been updated by playing genome.
+            const editRepEl = el.shadowRoot
+                ?.querySelector(EditRepresentativeElementStub.is) as EditRepresentativeElementStub
+            editRepEl.dispatchEvent(new CustomEvent('play'))
+            await elementUpdated(el)
+            expect(playbackSpy.setupSequence.calls.mostRecent().args[0].options).toEqual(newOptions)
+        })
+
+        it('should close popup', async () => {
+            const newOptions: PlaybackOptions = {
+                bpm: 1,
+                instrument: Instrument.Synth,
+                loop: true
+            }
+            playbackOptionsEl.dispatchEvent(new FormSubmitEvent<PlaybackOptions>({ value: newOptions }))
+            await elementUpdated(el)
+
+            expect(el.shadowRoot?.querySelector(PlaybackOptionsElementStub.is)).toBeFalsy()
+        })
+    })
+
+    describe('playback options cancel event', () => {
+        it('should close popup', async () => {
+            initiateWithGenomes([ [ 1 ] ], 'a')
+            await elementUpdated(el)
+            const headerEl = el.shadowRoot
+                ?.querySelector(RepresentativesHeaderElementStub.is) as RepresentativesHeaderElementStub
+            headerEl.dispatchEvent(new CustomEvent<PopupContent>('show-popup', { detail: 'playback' }))
+            await elementUpdated(el)
+            const playbackOptionsEl = el.shadowRoot
+                ?.querySelector(PlaybackOptionsElementStub.is) as PlaybackOptionsElementStub
+
+            playbackOptionsEl.dispatchEvent(new CustomEvent(cancelEventType))
+            await elementUpdated(el)
+
+            expect(el.shadowRoot?.querySelector(PlaybackOptionsElementStub.is)).toBeFalsy()
         })
     })
 })
