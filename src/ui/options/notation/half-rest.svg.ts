339a9705d70d0f328b5e062a8af65b34b386b4de
rferg <rmferguson77@gmail.com>
2020-10-15 13:14:05 -0400

    add music notation svgs

---
+++ src/ui/options/notation/half-rest.svg.ts
@@ -0,0 +1,6 @@
+import { html } from 'lit-element'
+
+export const halfRest = html`
+    <path d="M10.902164 18.913252h9.2204475v5.1031408H10.902164z"></path>
+    <path d="M7.9446626 22.972569h15.077462v1.1018145H7.9446626z"></path>
+`
---
+++ src/ui/options/notation/whole-rest.svg.ts
@@ -0,0 +1,6 @@
+import { html } from 'lit-element'
+
+export const wholeRest = html`
+    <path d="M10.902164 24.074383h9.2204475v-5.1031408H10.902164z"></path>
+    <path d="M7.9446626 20.015066h15.077462v-1.1018145H7.9446626z"></path>
+    `
