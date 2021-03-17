554f0e9f1a204af4e6f0d4e736ee7d8cc5fdddfe
rferg <rmferguson77@gmail.com>
2021-03-17 17:32:51 -0400

    (WIP) naive implementations of suffix array and LCP construction

--- src/genetic-algorithm/fitness/repeated-sequences-factory.ts
+++ src/genetic-algorithm/fitness/repeated-sequences-factory.ts
@@ -10,34 +10,38 @@ interface RepeatedSequencesOptions {
 }
 
 enum SequenceType {
-    Pitch = 1 << 0,
-    Rhythm = 1 << 1
+    Pitch,
+    Rhythm
 }
 
-export function repeatedSequencesFactory({
-    minLength,
-    maxLength,
-    type
-}: RepeatedSequencesOptions): (population: Population) => Int8Array {
+export function repeatedSequencesFactory(options: RepeatedSequencesOptions[]): (population: Population) => Int8Array {
     return (population: Population) => {
-        if (minLength || maxLength < 2) {
-            throw new Error('minLength and maxLength must be >= 2')
-        }
-        if (minLength > maxLength) {
-            throw new Error('minLength cannot be greater than maxLength')
-        }
-        if (maxLength >= population.genomeSize) {
-            throw new Error('maxLength cannot be greater than or equal to genomeSize')
-        }
         const results = new Int8Array(population.size)
-        // Calculate max possible score:
-        const maxPossibleScore = getMaxScore(minLength, maxLength)
-        for (let genomeIndex = 0; genomeIndex < population.size; genomeIndex++) {
-            const genome = population.get(genomeIndex)
-            const score = getScore(genome, { minLength, maxLength, type })
-            const fitness = Math.round((score / maxPossibleScore) * 100)
-            results[genomeIndex] = fitness
-        }
+
+        options.forEach(({
+            minLength,
+            maxLength,
+            type
+        }) => {
+            if (minLength || maxLength < 2) {
+                throw new Error('minLength and maxLength must be >= 2')
+            }
+            if (minLength > maxLength) {
+                throw new Error('minLength cannot be greater than maxLength')
+            }
+            if (maxLength >= population.genomeSize) {
+                throw new Error('maxLength cannot be greater than or equal to genomeSize')
+            }
+            // Calculate max possible score:
+            const maxPossibleScore = getMaxScore(minLength, maxLength)
+            for (let genomeIndex = 0; genomeIndex < population.size; genomeIndex++) {
+                const genome = population.get(genomeIndex)
+                const score = getScore(genome, { minLength, maxLength, type })
+                const fitness = Math.round((score / maxPossibleScore) * 100)
+                results[genomeIndex] += fitness
+            }
+        })
+
         return results
     }
 }
@@ -52,9 +56,10 @@ function getSumOfNumbersUpToN(n: number): number {
 
 function getScore(genome: Uint8Array, { minLength, maxLength, type }: RepeatedSequencesOptions): number {
     const encodedGenome = encodeGenome(genome, type)
-    // 1.   Construct LCP array according to type
-    // 2.   Add values that are length l, where minLength <= l <= maxLength.
-    throw new Error('not implemented' + genome + minLength + maxLength + type + encodedGenome)
+    const lcpArray = getLcpArray(encodedGenome)
+    return lcpArray
+        .filter(prefixLength => minLength <= prefixLength && prefixLength <= maxLength)
+        .reduce((prev, curr) => prev + curr, 0)
 }
 
 function encodeGenome(genome: Uint8Array, type: SequenceType): number[] {
@@ -65,12 +70,7 @@ function encodeGenome(genome: Uint8Array, type: SequenceType): number[] {
         return getRhythms(genome)
     }
 
-    const rhythms = getRhythms(genome)
-    const pitches = Array.from(genome.map(gene => GeneUtil.getPitch(gene as Uint8)))
-    return rhythms.map((rhythm, i) => {
-        const pitch = pitches[i]
-        return mapPairing(pitch, rhythm)
-    })
+    throw new Error(`Invalid sequence type ${type}`)
 }
 
 function getRhythms(genome: Uint8Array): number[] {
@@ -103,7 +103,50 @@ function getRhythms(genome: Uint8Array): number[] {
     return results
 }
 
-// see https://stackoverflow.com/a/13871379
-function mapPairing(a: number, b: number): number {
-    return a >= b ? a * a + a + b : a + b * b
+// We use naive, slow approach.
+// If necessary, we can optimize further using Kasai algorithm.
+function getLcpArray(encodedGenome: number[]): number[] {
+    const suffixArray = getSuffixArray(encodedGenome)
+    const lcp = []
+    for (let suffixIndex = 0; suffixIndex < suffixArray.length; suffixIndex++) {
+        let commonPrefixLength = 0
+        if (suffixIndex) {
+            const previous = encodedGenome.slice(suffixArray[suffixIndex - 1])
+            const current = encodedGenome.slice(suffixArray[suffixIndex])
+            for (let itemIndex = 0; itemIndex < current.length; itemIndex++) {
+                if (previous[itemIndex] !== current[itemIndex]) {
+                    break
+                }
+                commonPrefixLength++
+            }
+        }
+        lcp.push(commonPrefixLength)
+    }
+    return lcp
+}
+
+// We use a naive, slow approach to constructing suffix array.
+// If this is causing performance issues, we can optimize further (to O(n))
+function getSuffixArray(encodedGenome: number[]): number[] {
+    const suffixes: { suffix: number[], index: number }[] = []
+    for (let i = 0; i < encodedGenome.length; i++) {
+        suffixes.push({ index: i, suffix: encodedGenome.slice(i) })
+    }
+    suffixes.sort((a, b) => sortArraysOfNumbers(a.suffix, b.suffix))
+    return suffixes.map(({ index }) => index)
+}
+
+function sortArraysOfNumbers(a: number[], b: number[]): number {
+    const length = a.length > b.length ? a.length : b.length
+    for (let i = 0; i < length; i++) {
+        if (i >= b.length) { return 1 }
+        if (i >= a.length) { return -1 }
+        const aElement = a[i]
+        const bElement = b[i]
+        if (aElement === bElement) {
+            continue
+        }
+        return aElement - bElement
+    }
+    return 0
 }
